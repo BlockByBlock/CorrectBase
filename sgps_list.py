609adf3f78b244fddee7d0fa01f69964992bd761
Bing <ybingcheng@gmail.com>
2018-01-08 15:53:28 +0800

    Str and list format

--- sgps_list.py
+++ sgps_list.py
@@ -27,25 +27,27 @@ while(1):
 		us_satrss = []
 		cn_satid = []
 		cn_satrss = []
+		satnum = []
 
 		sdata =  read.split(',')
 		#print(sdata)
 
 		for i, j in enumerate(sdata):
 			if j == '$PUBX':
-				satnum = 'n' + sdata[i+2]
+				satnum.append(sdata[i+2])
 			if j == 'U':
-				us_satid.append('u' + sdata[i-1])
-				us_satrss.append('s' + sdata[i+3])
+				us_satid.append(sdata[i-1])
+				us_satrss.append(sdata[i+3])
 			if j == 'U' and len(sdata[i-1]) > 2 :
-				cn_satid.append('c' + sdata[i-1])
-				cn_satrss.append('s' + sdata[i+3])
+				cn_satid.append(sdata[i-1])
+				cn_satrss.append(sdata[i+3])
 
-		satid = ''.join(us_satid[:4] + cn_satid[:2])
-		satrss = ''.join(us_satrss[:4] + cn_satrss[:2])
+		satid = us_satid[:4] + cn_satid[:2]
+		satrss = us_satrss[:4] + cn_satrss[:2]
 
-		satinfo = str(satnum + satid + satrss)
-		print(satinfo)
+		print(satnum)
+		print(satid)
+		print(satrss)
 
 		#dList = [satnum, final_satid, final_satrss]	
 		#objList = pickle.dumps ( dList )
