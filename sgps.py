56518da557f5658f7841f58290c095080054446a
Bing <ybingcheng@gmail.com>
2018-01-09 09:57:06 +0800

    added , to separate key

--- sgps.py
+++ sgps.py
@@ -33,18 +33,17 @@ while(1):
 
 		for i, j in enumerate(sdata):
 			if j == '$PUBX':
-				satnum = 'n' + sdata[i+2]
+				satnum = 'n' + sdata[i+2] 
 			if j == 'U':
-				us_satid.append('u' + sdata[i-1])
-				us_satrss.append('s' + sdata[i+3])
+				us_satid.append(',u' + sdata[i-1])
+				us_satrss.append(',s' + sdata[i+3])
 			if j == 'U' and len(sdata[i-1]) > 2 :
-				cn_satid.append('c' + sdata[i-1])
-				cn_satrss.append('s' + sdata[i+3])
+				cn_satid.append(',c' + sdata[i-1])
+				cn_satrss.append(',s' + sdata[i+3])
 
-		satid = ''.join(us_satid[:4] + cn_satid[:2])
-		satrss = ''.join(us_satrss[:4] + cn_satrss[:2])
+		idrss = ''.join(us_satid[:4] + cn_satid[:2] + us_satrss[:4] + cn_satrss[:2])
 
-		satinfo = str(satnum + satid + satrss)
+		satinfo = str(satnum + idrss)
 		print(satinfo)
 
 		#dList = [satnum, final_satid, final_satrss]	
